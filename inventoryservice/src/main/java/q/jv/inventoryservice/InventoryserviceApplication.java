143052ec4ff3e91b9e5e5253b20d0b86f8320f6f
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-18 17:20:34 +0100

    Added sample data in the Inventory Micro-Service

--- inventoryservice/src/main/java/q/jv/inventoryservice/InventoryserviceApplication.java
+++ inventoryservice/src/main/java/q/jv/inventoryservice/InventoryserviceApplication.java
@@ -1,7 +1,13 @@
 package q.jv.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import q.jv.inventoryservice.entities.Product;
+import q.jv.inventoryservice.repository.ProductRepository;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryserviceApplication {
@@ -9,5 +15,27 @@ public class InventoryserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryserviceApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner init(ProductRepository productRepository) {
+        return args -> {
+          productRepository.save(Product.builder()
+                  .id(UUID.randomUUID().toString())
+                  .name("Ink")
+                  .price(255.0)
+                  .quantity(25)
+                  .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("Printer")
+                    .price(4000)
+                    .quantity(10)
+                    .build());
+            productRepository.save(Product.builder()
+                    .id(UUID.randomUUID().toString())
+                    .name("Computer")
+                    .price(5000)
+                    .quantity(8)
+                    .build());
+        };
+    }
 }
