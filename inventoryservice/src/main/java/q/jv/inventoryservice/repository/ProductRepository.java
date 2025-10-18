f4e1c1aca626b0de51178dbd840de5a3a686b78e
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-18 17:17:45 +0100

    Added a Product repository in the Inventory Micro-Service

---
+++ inventoryservice/src/main/java/q/jv/inventoryservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package q.jv.inventoryservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import q.jv.inventoryservice.entities.Product;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
