bdba9feb5796bc813283639f2f231e049f34e5c4
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-18 16:37:22 +0100

    Added a sample data in the Customer Micro-Service

--- customer-service/src/main/java/q/jv/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/q/jv/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package q.jv.customerservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import q.jv.customerservice.entities.Customer;
+import q.jv.customerservice.repository.CustomerRepository;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +13,21 @@ public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("Youssef")
+                    .email("ysf@email.mail")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("test")
+                    .email("tst@email.mail")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("test2")
+                    .email("tst@email.mail")
+                    .build());
+        };
+    }
 }
