e1450e83e01cec854f9be343befb910d6bcba9e2
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-10-18 17:13:41 +0100

    Added an Inventory Micro-Service

---
+++ inventoryservice/src/test/java/q/jv/inventoryservice/InventoryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package q.jv.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
